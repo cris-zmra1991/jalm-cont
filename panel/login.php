07e78f951aeeb45217097811c020ce2ec5b408f9
cris-zmra1991 <cris.zmra1991@gmail.com>
2022-01-11 15:36:31 -0500

    Actualizado form action y los nombres de los campos.

--- panel/login.php
+++ panel/login.php
@@ -99,17 +99,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
   <main class="form-signin">
-    <form>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
       <?php echo $errorconexion; ?>
       <img class="mb-4" src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
       <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
       <div class="form-floating">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <input type="email" class="form-control" id="floatingInput" name="user" placeholder="name@example.com">
         <label for="floatingInput">Email address</label>
       </div>
       <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
         <label for="floatingPassword">Password</label>
       </div>
 
