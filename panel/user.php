e3ed5b42e5c424e12f3252dc84f8fe3a54a71382
cris-zmra1991 <cris.zmra1991@gmail.com>
2022-01-11 17:47:44 -0500

    incluido script del grafico Chrt.js en el cuerpo del panel de usuario

--- panel/user.php
+++ panel/user.php
@@ -107,6 +107,70 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
-    <script src="../res/dashboard.js"></script>
+    
+    <script>
+    /* globals Chart:false, feather:false */
+
+    (function () {
+      'use strict'
+
+      feather.replace({ 'aria-hidden': 'true' })
+
+      // Graphs
+      var ctx = document.getElementById('myChart')
+      // eslint-disable-next-line no-unused-vars
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday'
+          ],
+          datasets: [{
+            data: [
+              15339,
+              21345,
+              18483,
+              24003,
+              23489,
+              24092,
+              12034
+            ],
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#007bff',
+            borderWidth: 4,
+            pointBackgroundColor: '#007bff'
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          },
+          legend: {
+            display: false
+          }
+        }
+      })
+    })()
+
+    function cargarDatos() {
+      $.ajax({
+        url:'../res/datos-dashboard.php',
+        type:'POST'
+      }).done(function responder() {
+        alert(responder);
+      })
+    }
+    </script>
   </body>
 </html>
