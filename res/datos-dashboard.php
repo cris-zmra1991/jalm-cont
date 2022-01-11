6c837dfe615190310ff51205b3a4ae622c54b566
cris-zmra1991 <cris.zmra1991@gmail.com>
2022-01-11 17:41:32 -0500

    Act

--- res/datos-dashboard.php
+++ res/datos-dashboard.php
@@ -1,9 +1,5 @@
 <?php
   $data = $database->select("users","*",["username" => $usuario]);
-  $arreglo = array();
-  while($grafico = mysqli_fetch_array($data)){
-    $arreglo[] = $grafico;
-  }
-  return $arreglo;
-  echo json_encode($arreglo);
+
+  echo json_encode($data);
 ?>
