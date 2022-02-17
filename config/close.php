d45d6c0b5e20fdf783665ffa2b8c40a985cc503f
cris-zmra1991 <cris.zmra1991@gmail.com>
2022-02-17 18:50:54 -0500

    Eliminado todo el contenido para comenzar nueva version

--- config/close.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'base.php';
-$database->update("users",["active" => "0"],["user_id" => $_SESSION['id_user']]);
-session_destroy();
-header('Location: '.RUTA.'index.php');
-?>
--- res/controladorgrafico.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'modelografico.php';
-$MG = new modelografico();
-$consulta = $MG -> TraerDatos();
-echo json_encode($consulta);
- ?>
