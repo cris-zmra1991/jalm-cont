8ba9c9ece83c06d8ffc2b0a870f3be5e3f23d9f8
cris-zmra1991 <cris.zmra1991@gmail.com>
2022-02-17 19:04:53 -0500

    Cambio en servidor de prueba

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 require 'res/php/app_top.php';
 $errores = "";
 if(!isset($_SESSION['user'])){
 	//REDIRIGIR Al PROCESO DE LOGIN
-	header('Location: http://localhost:8030/new-app/acceso');
+	header('Location: https://jalm-cont.herokuapp.com/acceso');
 }
 elseif(isset($_SESSION['user'])){
 	//VERIFICAR EL TIPO DE USUARIO
-	
+
 }
 else{
 window.alert("A ocurrido un error. Recargue la página y vuelva a intentarlo");
